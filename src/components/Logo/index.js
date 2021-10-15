c413ba5ffc68ac6bb5416e3252c1bf8347960f3a
JardanCarvalho <jardan.carvalho@gmail.com>
2021-10-15 20:27:22 -0300

    fix: removing double and changing alt content

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import './style.css';
 import logo from '../../images/logo.svg';
 
 const Logo = () => {
   return (
     <div>
-      <img className="logo-img" src={logo} alt="loubank" />
+      <img className="logo-img" src={logo} alt="luo bank" />
     </div>
   );
 }
