7192dcf0330d5cbb800c8dfe776bab370fee12b4
Giovanni Rosario <giovannisilvati@outlook.com>
2021-10-16 19:10:20 -0300

    feat: creating small credit card

---
+++ src/components/SmallCreditCard/SmallCreditCard.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SmallCreditCard from './';
+
+export default {
+  component: SmallCreditCard,
+  title: 'SmallCreditCard',
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+};
+
+export const Default = (args) => <SmallCreditCard {...args} />;
+
+Default.args = {
+  brand: "visa",
+  limitValue: 5000,
+  cardNumber: 6969
+}
---
+++ src/components/SmallCreditCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+import { SiMastercard, SiVisa, SiAmericanexpress } from "react-icons/si";
+
+const cardBrands = {
+  visa: <SiVisa />,
+  mastercard: <SiMastercard />,
+  americanexpress: <SiAmericanexpress />
+}
+
+export default ({ brand, limitValue, cardNumber }) => {
+  return (
+    <div className={`small-creditcard-container brand-${brand}`} >
+      {cardBrands[brand]}
+      <div className="small-creditcard-limit">
+        <span>Limite</span>
+        <span>R$ {limitValue.toLocaleString()}</span>
+      </div>
+      <span>*** {cardNumber}</span>
+    </div>
+  );  
+}
